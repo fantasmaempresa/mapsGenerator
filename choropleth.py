8479c85b4cbab94915602ff13cc2b752f054b728
Erick Dylan Navarrete de la Vega <ericknavarrete55@gmail.com>
2022-10-31 22:42:23 -0600

    "Prueba mapa"

--- choropleth.py
+++ choropleth.py
@@ -1,18 +1,16 @@
-import argparse
-import csv
-import re
 from unidecode import unidecode
-import json
 from ast import parse
 from email.policy import default
 
+import json
+import argparse
 import geopandas
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-import shapefile
 
 mapbox_access_token = 'pk.eyJ1IjoiZXJpY2tuYXZlIiwiYSI6ImNrdDh3MnppbzE2NXIydm5ybWtkNjM5Y3UifQ.4bfoMQc-wQkbI7f2YQRhUA'
+
 
 def createMap():
     municipalityShp = geopandas.read_file("assets/Tabasco/mun.shp")
@@ -32,12 +30,22 @@ def createMap():
     fig.show()
     fig.write_html("file.html")
 
+
 def printRow(row):
     return (row['TOTAL DE VOTOS'] * 100) / row['LISTA NOMINAL']
 
+def getWinner(row, politcParties):
+    
+    dictWinners = {}
+    for key in politcParties:
+        dictWinners[key] = row[key]
+
+    return max(dictWinners, key=dictWinners.get)
+
 def replaceContent(row):
     try:
-        row = row.str.normalize('NFKD').str.encode('ascii',errors='ignore').str.decode('utf-8')
+        row = row.str.normalize('NFKD').str.encode(
+            'ascii', errors='ignore').str.decode('utf-8')
     except Exception as e:
         row
 
@@ -45,26 +53,27 @@ def replaceContent(row):
 
 
 def createSectionMap(pathGeojson: str, pathData: str, queryString: str, keyToMap: str, locations: str, featureidKey: str, additionalData: str, queryDF: str):
-    
+
     if additionalData == '':
         hoverData = []
     else:
         hoverData = additionalData.split(',')
-        
+
     municipalityShp = geopandas.read_file(pathGeojson)
     if queryString != "":
         municipalityShp.query(queryString, inplace=True)
     municipalityShp.to_file('myJson.geojson', driver='GeoJSON')
     municipalityGeoJson = json.load(open('myJson.geojson'))
-    
+
     df = pd.read_csv(pathData)
     if queryDF != "":
-        df.query(queryDF, inplace=True) 
+        df.query(queryDF, inplace=True)
 
-    db = df.groupby(['SECCION ELECTORAL']).sum(numeric_only=True).T.T.reset_index()
-    db['% PARTICIPACION'] = db.apply(lambda row : printRow(row), axis=1)
+    db = df.groupby(['SECCION ELECTORAL']).sum(
+        numeric_only=True).T.T.reset_index()
+    db['% PARTICIPACION'] = db.apply(lambda row: printRow(row), axis=1)
 
-    fig = px.choropleth(db, geojson=municipalityGeoJson, color=keyToMap,
+    fig = px.choropleth(db, geojson=municipalityGeoJson, color=['PAN', 'PRI'],
                         locations=locations, featureidkey=featureidKey,
                         color_continuous_scale="PuRd",
                         hover_data=hoverData,
@@ -76,6 +85,30 @@ def createSectionMap(pathGeojson: str, pathData: str, queryString: str, keyToMap
     fig.write_html("file.html")
     fig.show()
 
+def comparisonMap():
+    municipalityShp = geopandas.read_file('assets/Tabasco/secc.shp')
+    municipalityShp.query("entidad == 27", inplace=True)
+    municipalityShp.to_file('myJson.geojson', driver='GeoJSON')
+    municipalityGeoJson = json.load(open('myJson.geojson'))
+
+    df = pd.read_csv("mapas/2020-2021/csv/ayu_resumen_general.csv")
+    # df.query("MUNICIPIO == 'BALANCAN'", inplace=True)
+
+    db = df.groupby(['SECCION ELECTORAL']).sum(
+        numeric_only=True).T.T.reset_index()
+    db['% PARTICIPACION'] = db.apply(lambda row: printRow(row), axis=1)
+    db['WINNER'] = db.apply(lambda row: getWinner(row, ['PAN','PT','PRD','PRI','MORENA']), axis=1)
+
+    fig = px.choropleth_mapbox(db, geojson=municipalityGeoJson, color="WINNER",
+                           locations="SECCION ELECTORAL", featureidkey="properties.seccion",
+                           color_continuous_scale="PuRd",
+                           mapbox_style="carto-positron", zoom=9)
+
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.update_layout(margin={"r": 100, "t": 100, "l": 100, "b": 100})
+    fig.write_html("file.html")
+    fig.show()
+    
 
 def main():
     parser = argparse.ArgumentParser(description="Render maps of votes")
@@ -96,4 +129,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    comparisonMap()
