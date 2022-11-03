b64c5deac8888cd8c0f7d0183d67cd9a2edcbd3b
Erick Dylan Navarrete de la Vega <ericknavarrete55@gmail.com>
2022-11-02 18:03:52 -0600

    :sparkles: Introducimos DASH y agregamos Distrito Local, Federal

--- choropleth.py
+++ choropleth.py
@@ -1,6 +1,7 @@
 from unidecode import unidecode
 from ast import parse
 from email.policy import default
+from dash import Dash, html, dcc
 
 import json
 import argparse
@@ -34,13 +35,21 @@ def createMap():
 def printRow(row):
     return (row['TOTAL DE VOTOS'] * 100) / row['LISTA NOMINAL']
 
+
 def getWinner(row, politcParties):
-    
+
     dictWinners = {}
     for key in politcParties:
         dictWinners[key] = row[key]
 
     return max(dictWinners, key=dictWinners.get)
+
+
+def getMunicipality(row, df):
+    value = row['SECCION ELECTORAL']
+    query = df.query("`SECCION ELECTORAL` == @value")
+    return query.iloc[0]['MUNICIPIO']
+
 
 def replaceContent(row):
     try:
@@ -85,30 +94,258 @@ def createSectionMap(pathGeojson: str, pathData: str, queryString: str, keyToMap
     fig.write_html("file.html")
     fig.show()
 
+
 def comparisonMap():
+    municipalities = pd.read_csv("assets/Tabasco/municipios.csv")
     municipalityShp = geopandas.read_file('assets/Tabasco/secc.shp')
-    municipalityShp.query("entidad == 27", inplace=True)
-    municipalityShp.to_file('myJson.geojson', driver='GeoJSON')
-    municipalityGeoJson = json.load(open('myJson.geojson'))
+
+    municiplaitiesName = []
+    municiplaitiesGeometry = []
+
+    for index, row in municipalities.iterrows():
+        municiplaitiesName.append(row['municipio'])
+        municipality = row['clave']
+        query = municipalityShp.query(
+            "entidad == 27 & municipio == @municipality")
+        municiplaitiesGeometry.append(query['geometry'].unary_union)
+
+    data = {'municipio': municiplaitiesName,
+            'geometry': municiplaitiesGeometry
+            }
+
+    df_marques = pd.DataFrame(data)
+    municipalityGeoJson = geopandas.GeoDataFrame(df_marques, crs='epsg:4326')
+    municipalityGeoJson.to_file('myJson.geojson', driver='GeoJSON')
+    municipalityGeoJson = json.load(open('myJson.geojson', encoding="utf8"))
 
     df = pd.read_csv("mapas/2020-2021/csv/ayu_resumen_general.csv")
-    # df.query("MUNICIPIO == 'BALANCAN'", inplace=True)
 
-    db = df.groupby(['SECCION ELECTORAL']).sum(
+    db = df.groupby(['MUNICIPIO']).sum(
         numeric_only=True).T.T.reset_index()
+
+    # db['MUNICIPIO'] = db.apply(lambda row: getMunicipality(row, df), axis=1)
     db['% PARTICIPACION'] = db.apply(lambda row: printRow(row), axis=1)
-    db['WINNER'] = db.apply(lambda row: getWinner(row, ['PAN','PT','PRD','PRI','MORENA']), axis=1)
+    db['WINNER'] = db.apply(lambda row: getWinner(
+        row, ['PAN', 'PT', 'PRD', 'PRI', 'MORENA']), axis=1)
+
+    # fig = px.choropleth_mapbox(db, geojson=municipalityGeoJson, color='MORENA',
+    #                            locations='MUNICIPIO', featureidkey='properties.municipio',
+    #                            color_continuous_scale="PuRd",
+    #                            title='Información por sección', mapbox_style="carto-positron", zoom=9
+    #                            )
 
     fig = px.choropleth_mapbox(db, geojson=municipalityGeoJson, color="WINNER",
-                           locations="SECCION ELECTORAL", featureidkey="properties.seccion",
-                           color_continuous_scale="PuRd",
-                           mapbox_style="carto-positron", zoom=9)
+                               locations="MUNICIPIO", featureidkey="properties.municipio",
+                               color_continuous_scale="PuRd",
+                               mapbox_style="carto-positron", zoom=9)
 
     fig.update_geos(fitbounds="locations", visible=False)
     fig.update_layout(margin={"r": 100, "t": 100, "l": 100, "b": 100})
     fig.write_html("file.html")
     fig.show()
+
+
+def comparisonDistF():
+    municipalityShp = geopandas.read_file('assets/Tabasco/secc.shp')
+
+    municiplaitiesName = []
+    municiplaitiesGeometry = []
+
+    for row in range(6):
+        municiplaitiesName.append(row + 1)
+        municipality = row + 1
+        query = municipalityShp.query(
+            "entidad == 27 & distrito_f == @municipality")
+        municiplaitiesGeometry.append(query['geometry'].unary_union)
+
+    data = {'distrito_f': municiplaitiesName,
+            'geometry': municiplaitiesGeometry
+            }
+
+    df_marques = pd.DataFrame(data)
+    municipalityGeoJson = geopandas.GeoDataFrame(df_marques, crs='epsg:4326')
+    municipalityGeoJson.to_file('myJson.geojson', driver='GeoJSON')
+    municipalityGeoJson = json.load(open('myJson.geojson', encoding="utf8"))
+
+    df = pd.read_csv("mapas/2020-2021/csv/ayu_resumen_general.csv")
+
+    db = df.groupby(['DISTRITO F']).sum(
+        numeric_only=True).T.T.reset_index()
+
+    # db['MUNICIPIO'] = db.apply(lambda row: getMunicipality(row, df), axis=1)
+    db['% PARTICIPACION'] = db.apply(lambda row: printRow(row), axis=1)
+    db['WINNER'] = db.apply(lambda row: getWinner(
+        row, ['PAN', 'PT', 'PRD', 'PRI', 'MORENA']), axis=1)
+
+    # fig = px.choropleth_mapbox(db, geojson=municipalityGeoJson, color='MORENA',
+    #                            locations='MUNICIPIO', featureidkey='properties.municipio',
+    #                            color_continuous_scale="PuRd",
+    #                            title='Información por sección', mapbox_style="carto-positron", zoom=9
+    #                            )
+
+    fig = px.choropleth_mapbox(db, geojson=municipalityGeoJson, color="WINNER",
+                               locations="DISTRITO F", featureidkey="properties.distrito_f",
+                               color_continuous_scale="PuRd",
+                               mapbox_style="carto-positron", zoom=9)
+
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.update_layout(margin={"r": 100, "t": 100, "l": 100, "b": 100})
+    fig.write_html("file.html")
+    fig.show()
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+
+def comparisonDistL():
+    municipalityShp = geopandas.read_file('assets/Tabasco/secc.shp')
+
+    municiplaitiesName = []
+    municiplaitiesGeometry = []
+
+    for row in range(21):
+        municiplaitiesName.append(row + 1)
+        municipality = row + 1
+        query = municipalityShp.query(
+            "entidad == 27 & distrito_l == @municipality")
+        municiplaitiesGeometry.append(query['geometry'].unary_union)
+
+    data = {'distrito_l': municiplaitiesName,
+            'geometry': municiplaitiesGeometry
+            }
+
+    df_marques = pd.DataFrame(data)
+    municipalityGeoJson = geopandas.GeoDataFrame(df_marques, crs='epsg:4326')
+    municipalityGeoJson.to_file('myJson.geojson', driver='GeoJSON')
+    municipalityGeoJson = json.load(open('myJson.geojson', encoding="utf8"))
+
+    df = pd.read_csv("mapas/2020-2021/csv/ayu_resumen_general.csv")
+
+    db = df.groupby(['DISTRITO L']).sum(
+        numeric_only=True).T.T.reset_index()
+
+    # db['MUNICIPIO'] = db.apply(lambda row: getMunicipality(row, df), axis=1)
+    db['% PARTICIPACION'] = db.apply(lambda row: printRow(row), axis=1)
+    db['WINNER'] = db.apply(lambda row: getWinner(
+        row, ['PAN', 'PT', 'PRD', 'PRI', 'MORENA']), axis=1)
+
+    fig = px.choropleth_mapbox(db, geojson=municipalityGeoJson, color="WINNER",
+                               locations="DISTRITO L", featureidkey="properties.distrito_l",
+                               color_discrete_map={
+                                   'PAN': '#00008B ',	'PRI': '#008000',	'PRD': '#FFD700',	'PVEM': '#00FF00',	'PT': '#B22222',
+                                   'MC': '#D2691E',	'MORENA': '#621132',	'PES': '#8B008B',	'RSP': '#8B0000',	'FPM': '#FF1493'
+                               },
+                               mapbox_style="carto-positron", zoom=7, height=800, center={
+                                   'lat': 18.028157,
+                                   'lon': -92.753621
+                               })
+
+    # fig.update_geos(fitbounds="locations", visible=False)
+    # fig.write_html("file.html")
+    # fig.show()
+
+    return fig
+
+
+def page():
+    app = Dash(__name__)
+    df = pd.read_csv("mapas/2020-2021/csv/ayu_resumen_general.csv")
+
+    db = df.groupby(['DISTRITO L']).sum(
+        numeric_only=True).T.T.reset_index()
+
+    db['% PARTICIPACION'] = db.apply(lambda row: printRow(row), axis=1)
     
+    db.drop('SECCION ELECTORAL', axis=1, inplace=True)
+    db.drop('DISTRITO F', axis=1, inplace=True)
+
+    parties = ['PAN',	'PRI',	'PRD',	'PVEM',	'PT',
+               'MC',	'MORENA',	'PES',	'RSP',	'FPM']
+
+    votes = []
+    politicalParties = []
+    district = []
+
+    for index, row in db.iterrows():
+        district.append(row['DISTRITO L'])
+        district.append(row['DISTRITO L'])
+        district.append(row['DISTRITO L'])
+        district.append(row['DISTRITO L'])
+        district.append(row['DISTRITO L'])
+        district.append(row['DISTRITO L'])
+        district.append(row['DISTRITO L'])
+        district.append(row['DISTRITO L'])
+        district.append(row['DISTRITO L'])
+        district.append(row['DISTRITO L'])
+
+        for item in parties:
+            votes.append(row[item])
+            politicalParties.append(item)
+
+    finalData = pd.DataFrame({
+        "Partido": politicalParties,
+        "Distrito Local": district,
+        "Votos": votes
+    })
+
+    fig = px.bar(finalData, x="Distrito Local", y="Votos",
+                 color="Partido", barmode="group", color_discrete_map={
+                     'PAN': '#00008B ',	'PRI': '#008000',	'PRD': '#FFD700',	'PVEM': '#00FF00',	'PT': '#B22222',
+                     'MC': '#D2691E',	'MORENA': '#621132',	'PES': '#8B008B',	'RSP': '#8B0000',	'FPM': '#FF1493'
+                 }, height=700)
+
+    app.layout = html.Div(
+        children=[
+            html.Div(
+                id="banner",
+                className="banner",
+                children=[
+                    html.H2("Análisis por Distritos Locales"),
+                ],
+            ),
+            html.Div(
+                id="upper-container",
+                className="row",
+                children=[
+                    html.Div(
+                        id="geo-map-outer",
+                        children=[
+                            html.Div(
+                                id="geo-map-loading-outer",
+                                children=[
+                                    dcc.Loading(
+                                        id="loading",
+                                        children=dcc.Graph(
+                                            id="geo-map",
+                                            figure=comparisonDistL(),
+                                        ),
+                                    )
+                                ],
+                            ),
+                            dcc.Graph(
+                                id='example-graph',
+                                figure=fig
+                            ),
+                            generate_table(db,len(db.index))
+                        ],
+                    ),
+                ],
+            ),
+        ],
+    )
+
+    app.run_server(debug=True)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Render maps of votes")
@@ -129,4 +366,4 @@ def main():
 
 
 if __name__ == "__main__":
-    comparisonMap()
+    page()
